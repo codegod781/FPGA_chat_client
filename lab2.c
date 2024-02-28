25f7762218deb5140f62f9f550f1c5fa3f5bdc5c
Dan Ivanovich <dan@ivanovi.ch>
2024-02-28 05:02:35 +0000

    Add layout line

--- lab2.c
+++ lab2.c
@@ -2,17 +2,18 @@
  * lab2.c
  * CSEE 4840 Lab 2 for Spring 2024
  *
- * Names/UNIs: Patrick Cronin (pjc2192), Kiryl Beliauski (kb3338), Daniel Ivanovich (dmi2115)
+ * Names/UNIs: Patrick Cronin (pjc2192), Kiryl Beliauski (kb3338), Daniel
+ * Ivanovich (dmi2115)
  */
 #include "fbputchar.h"
+#include "usbkeyboard.h"
+#include <arpa/inet.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
 #include <unistd.h>
-#include "usbkeyboard.h"
-#include <pthread.h>
 
 /* Update SERVER_HOST to be the IP address of
  * the chat server you are connecting to
@@ -23,13 +24,15 @@
 
 #define BUFFER_SIZE 128
 
+#define DIVIDING_LINE '-'
+
 /*
  * References:
  *
  * https://web.archive.org/web/20130307100215/http://beej.us/guide/bgnet/output/html/singlepage/bgnet.html
  *
  * http://www.thegeekstuff.com/2011/12/c-socket-programming/
- * 
+ *
  */
 
 int sockfd; /* Socket file descriptor */
@@ -40,9 +43,17 @@ uint8_t endpoint_address;
 pthread_t network_thread;
 void *network_thread_f(void *);
 
-int main()
-{
-  int err, col;
+/*
+ * Runs a layout initialization: sets a dividing line down the third row from
+ * the bottom
+ */
+void draw_layout() {
+  for (int i = 0; i < 64; i++)
+    fbputchar(DIVIDING_LINE, 21, i);
+}
+
+int main() {
+  int err;
 
   struct sockaddr_in serv_addr;
 
@@ -55,22 +66,16 @@ int main()
     exit(1);
   }
 
-  /* Draw rows of asterisks across the top and bottom of the screen */
-  for (col = 0 ; col < 64 ; col++) {
-    fbputchar('*', 0, col);
-    fbputchar('*', 23, col);
-  }
-
-  fbputs("Hello CSEE 4840 World!", 4, 10);
+  fbclear();
 
   /* Open the keyboard */
-  if ( (keyboard = openkeyboard(&endpoint_address)) == NULL ) {
+  if ((keyboard = openkeyboard(&endpoint_address)) == NULL) {
     fprintf(stderr, "Did not find a keyboard\n");
     exit(1);
   }
-    
+
   /* Create a TCP communications socket */
-  if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
+  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     fprintf(stderr, "Error: Could not create socket\n");
     exit(1);
   }
@@ -79,13 +84,13 @@ int main()
   memset(&serv_addr, 0, sizeof(serv_addr));
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_port = htons(SERVER_PORT);
-  if ( inet_pton(AF_INET, SERVER_HOST, &serv_addr.sin_addr) <= 0) {
+  if (inet_pton(AF_INET, SERVER_HOST, &serv_addr.sin_addr) <= 0) {
     fprintf(stderr, "Error: Could not convert host IP \"%s\"\n", SERVER_HOST);
     exit(1);
   }
 
   /* Connect the socket to the server */
-  if ( connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+  if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
     fprintf(stderr, "Error: connect() failed.  Is the server running?\n");
     exit(1);
   }
@@ -93,18 +98,21 @@ int main()
   /* Start the network thread */
   pthread_create(&network_thread, NULL, network_thread_f, NULL);
 
+  /* Init layout */
+  draw_layout();
+
   /* Look for and handle keypresses */
   for (;;) {
     libusb_interrupt_transfer(keyboard, endpoint_address,
-			      (unsigned char *) &packet, sizeof(packet),
-			      &transferred, 0);
+                              (unsigned char *)&packet, sizeof(packet),
+                              &transferred, 0);
     if (transferred == sizeof(packet)) {
       sprintf(keystate, "%02x %02x %02x", packet.modifiers, packet.keycode[0],
-	      packet.keycode[1]);
+              packet.keycode[1]);
       printf("%s\n", keystate);
-      fbputs(keystate, 6, 0);
+      // fbputs(keystate, 6, 0);
       if (packet.keycode[0] == 0x29) { /* ESC pressed? */
-	break;
+        break;
       }
     }
   }
@@ -118,17 +126,15 @@ int main()
   return 0;
 }
 
-void *network_thread_f(void *ignored)
-{
+void *network_thread_f(void *ignored) {
   char recvBuf[BUFFER_SIZE];
   int n;
   /* Receive data */
-  while ( (n = read(sockfd, &recvBuf, BUFFER_SIZE - 1)) > 0 ) {
+  while ((n = read(sockfd, &recvBuf, BUFFER_SIZE - 1)) > 0) {
     recvBuf[n] = '\0';
     printf("%s", recvBuf);
-    fbputs(recvBuf, 8, 0);
+    // fbputs(recvBuf, 8, 0);
   }
 
   return NULL;
 }
-
